e50a26dcb4b30c39500f36a1371669644807e94f
Gautam Mekkat <12224008+Blue9@users.noreply.github.com>
2023-07-16 20:10:56 -0400

    Initial Commit

---
+++ news/newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApp.swift
+//  news
+//
+//  Created by Gautam Mekkat on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
